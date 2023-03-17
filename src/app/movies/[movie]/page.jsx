e01e8a8aa38f140487726d39bbb04909df25a4f0
Vishal-HFN <vishal.prakash@volunteer.heartfulness.org>
2023-03-17 16:14:14 +0530

    worked on movies page

---
+++ src/app/movies/[movie]/page.jsx
@@ -0,0 +1,9 @@
+
+const Movie = ({ params }) => {
+  const { movie } = params
+  return (
+    <div>Movie</div>
+  )
+}
+
+export default Movie
--- src/app/page.js
+++ src/app/page.js
@@ -1,90 +1,38 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css"
+import Link from "next/link";
 
-const inter = Inter({ subsets: ['latin'] })
+async function revalidatedData() {
+  return await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=b95f73a43bb59aa4b0ca7ea43ad4cdcd`, {
+    next: { revalidate: 10 },
+  }).then(r => r.json());
+}
 
-export default function Home() {
+const IMG_BaseURL = "https://image.tmdb.org/t/p/w500";
+
+const imageUrl = (img) => `${IMG_BaseURL}${img}`;
+
+export default async function Home() {
+  const moviesList = await revalidatedData();
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <h1>Movies List</h1>
+      <div className={`${styles.grid_layout}`}>
+        {moviesList && moviesList.results.map(movie => <div className={`${styles.movies_wrapper}`} key={movie?.id}>
+          <div className={`${styles.image_wrapper}`}>
+            <Image src={imageUrl(movie.poster_path)} alt={movie?.title} width={250} height={350} />
+          </div>
+          <div className={`${styles.content_wrapper}`}>
+            <div className={`${styles.title}`}>
+              {movie?.title}
+            </div>
+            <div className={`${styles.desctription}`}>
+              {movie.overview}
+            </div>
+            <Link className={`${styles.know_more}`} href={`/movies/${movie.id}`}>Know more</Link>
+          </div>
+        </div>)}
       </div>
     </main>
   )
